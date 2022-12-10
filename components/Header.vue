4bc345c79353db3c8bd28c0f46cbf6278bb94b91
cblokkeel <cblokkeel@partner.auchan.fr>
2022-12-10 10:43:22 +0100

    feat: add logout button in header

--- components/Header.vue
+++ components/Header.vue
@@ -1,23 +1,28 @@
 <template>
   <header class="wrapper flex justify-between">
     <h1>header</h1>
-    <button
-      to="/login"
-      v-if="status === 'unauthenticated'"
-      @click="handleLogin"
-    >
+    <button to="/login" v-if="!isUserAuthanticated" @click="handleLogin">
       Login
     </button>
-    <h1 v-if="status === 'authenticated' && data && data.user">
-      {{ data.user.name }}
-    </h1>
+    <div v-if="isUserAuthanticated">
+      {{ data?.user?.name }}
+      <button @click="handleLogout">Logout</button>
+    </div>
   </header>
 </template>
 
 <script setup lang="ts">
-const { status, data, signIn } = useSession();
+const { status, data, signIn, signOut } = useSession();
 
 const handleLogin = async () => {
   await signIn('google');
 };
+
+const handleLogout = async () => {
+  await signOut();
+};
+
+const isUserAuthanticated = computed(() => {
+  return status.value === 'authenticated';
+});
 </script>
