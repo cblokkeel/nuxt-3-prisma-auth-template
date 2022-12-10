2c7880afb22cd6121ddbcb8892116fffb376bb02
cblokkeel <cblokkeel@partner.auchan.fr>
2022-12-10 11:00:48 +0100

    feat: add prisma and auth stuff in scheam

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,8 +1,11 @@
 import { NuxtAuthHandler } from '#auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import GoogleProvider from 'next-auth/providers/google';
+import { prismaClient } from '~~/lib/prisma';
 
 export default NuxtAuthHandler({
   secret: process.env.NUXT_SECRET,
+  adapter: PrismaAdapter(prismaClient),
   providers: [
     // @ts-ignore
     GoogleProvider.default({
